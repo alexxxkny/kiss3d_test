04f81cbd461c198c3dc1b7f23d9087e239f587d8
Danila Alekseev <alexxxkny@mail.ru>
2022-11-07 17:20:47 +0300

    rotation

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,48 @@
 extern crate kiss3d;
 extern crate nalgebra as na;
 
-use kiss3d::conrod::{self, widget};
+use kiss3d::conrod::{self, widget, UiCell};
 use kiss3d::light::Light;
 use kiss3d::window::Window;
+use kiss3d::camera::{FixedView, Camera};
 use na::{Translation3, UnitQuaternion, Vector3, Point3};
-use std::f32;
+
+use std::f32::consts::PI;
 use std::path::Path;
 
 use conrod::{Sizeable, Positionable, Labelable, Widget, widget_ids};
 
+const UI_W: f64 = 300.;
+
+struct Rotation {
+    x_angle: f32,
+    y_angle: f32,
+    z_angle: f32,
+}
+
+impl Rotation {
+    fn x(&self) -> UnitQuaternion<f32> {
+        UnitQuaternion::from_axis_angle(&Vector3::x_axis(), self.x_angle / 180.0 * PI)
+    }
+
+    fn y(&self) -> UnitQuaternion<f32> {
+        UnitQuaternion::from_axis_angle(&Vector3::y_axis(), self.y_angle / 180.0 * PI)
+    }
+
+    fn z(&self) -> UnitQuaternion<f32> {
+        UnitQuaternion::from_axis_angle(&Vector3::z_axis(), self.z_angle / 180.0 * PI)
+    }
+}
+
 widget_ids! {
     pub struct Ids {
         canvas,
-        range,
+        slider_x,
+        slider_y,
+        slider_z,
+        angle_x,
+        angle_y,
+        angle_z,
     }
 }
 
@@ -39,8 +68,69 @@ pub fn theme() -> conrod::Theme {
     }
 }
 
+// nomalized witdh
+fn w_n(target_w: f32, window_h: f32) -> f32 {
+    target_w / window_h
+}
+
+fn draw_ui(ui_cell: &mut UiCell, ids: &Ids, rot: &mut Rotation) {
+    widget::Canvas::new()
+        .align_left()
+        .w(UI_W)
+        .scroll_kids_vertically()
+        .set(ids.canvas, ui_cell);
+
+    for v in widget::Slider::new(rot.x_angle, -180.0, 180.0)
+        .label("X")
+        .w(200.)
+        .h(25.)
+        .mid_top_of(ids.canvas)
+        .set(ids.slider_x, ui_cell) 
+    {
+        rot.x_angle = v;
+    }
+
+    widget::Text::new(&rot.x_angle.to_string())
+        .right_from(ids.slider_x, 10.)
+        .set(ids.angle_x, ui_cell);
+
+    for v in widget::Slider::new(rot.y_angle, -180.0, 180.0)
+        .label("Y")
+        .w(200.)
+        .h(25.)
+        .down_from(ids.slider_x, 20.)
+        .set(ids.slider_y, ui_cell) 
+    {
+        rot.y_angle = v;
+    }
+
+    widget::Text::new(&rot.y_angle.to_string())
+        .right_from(ids.slider_y, 10.)
+        .set(ids.angle_y, ui_cell);
+
+    for v in widget::Slider::new(rot.z_angle, -180.0, 180.0)
+        .label("Z")
+        .w(200.)
+        .h(25.)
+        .down_from(ids.slider_y, 20.)
+        .set(ids.slider_z, ui_cell) 
+    {
+        rot.z_angle = v;
+    }
+
+    widget::Text::new(&rot.z_angle.to_string())
+        .right_from(ids.slider_z, 10.)
+        .set(ids.angle_z, ui_cell);
+}
+
 fn main() {
+    // Window
     let mut window = Window::new("Kiss3d: obj");
+    window.set_light(Light::StickToCamera);
+    window.set_background_color(1.0, 1.0, 1.0);
+
+    // State
+    let mut rotation = Rotation {x_angle: 0.0, y_angle: 0.0, z_angle: 0.0};
 
     // Teapot
     let obj_path = Path::new("./src/media/teapot.obj");
@@ -49,44 +139,20 @@ fn main() {
     println!("{:?}", mtl_path);
     let mut teapot = window.add_obj(obj_path, mtl_path, Vector3::new(0.001, 0.001, 0.001));
     
-    let rot = UnitQuaternion::from_axis_angle(&Vector3::z_axis(), 2.);
-    
-    //teapot.set_local_rotation(rot);
-    //teapot.set_local_translation(Translation3::new(-0.4, 0.0, 0.0));
-
-    window.set_light(Light::StickToCamera);
-    window.set_background_color(1.0, 1.0, 1.0);
-
-    let rot_teapot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+    // UI
+    window.conrod_ui_mut().theme = theme();
     let ids = Ids::new(window.conrod_ui_mut().widget_id_generator());
-    //window.conrod_ui_mut().theme = theme();
-    let ui_canvas_width = 300.0;
 
     while window.render() {
-        let window_width = window.width() as f64;
-        let mut ui = window.conrod_ui_mut().set_widgets();
+        let window_w = window.width() as f32;
+        let window_h = window.height() as f32;
 
-        let teapot_x_translation = (-1.0 + (1.0 * (ui_canvas_width / window_width) / 2.0)) as f32;
-        println!("{}", teapot_x_translation);
-        //teapot.set_local_translation(Translation3::new(teapot_x_translation, 0.0, 0.0));
-        teapot.set_local_translation(Translation3::new(2.0, 0.0, 0.0));
-
-        widget::Canvas::new()
-            .align_left()
-            .w(300.)
-            .scroll_kids_vertically()
-            .set(ids.canvas, &mut ui);
+        let teapot_x_translation = -w_n(UI_W as f32, window_h) / 2.;
         
-        widget::RangeSlider::new(1., 10., 1., 10.)
-            .label("X")
-            .w(200.)
-            .h(25.)
-            .mid_top_of(ids.canvas)
-            .set(ids.range, &mut ui);
+        teapot.set_local_translation(Translation3::new(teapot_x_translation, 0.0, 0.0));
+        teapot.set_local_rotation(rotation.x() * rotation.y() * rotation.z());
 
-        drop(ui);
-
-        window.draw_line(&Point3::new(0.0, 0.5, 0.0), &Point3::new(0.0, -0.5, 0.0), &Point3::new(0.0, 0.0, 0.0));
-        window.draw_line(&Point3::new(0.5, 0.0, 0.0), &Point3::new(-0.5, 0.0, 0.0), &Point3::new(0.0, 0.0, 0.0));
+        let mut ui_cell = window.conrod_ui_mut().set_widgets();
+        draw_ui(&mut ui_cell, &ids, &mut rotation);
     }
 }
