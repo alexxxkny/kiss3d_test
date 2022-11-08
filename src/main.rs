a096783a88c0da00e1bfbe8b8c369d3294dbb837
Danila Alekseev <alexxxkny@mail.ru>
2022-11-08 13:59:47 +0300

    done (almost)

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,24 @@ extern crate nalgebra as na;
 
 use kiss3d::conrod::{self, widget, UiCell, Colorable, Borderable};
 use kiss3d::light::Light;
-use kiss3d::text::Font;
 use kiss3d::window::Window;
-use kiss3d::camera::{FixedView, Camera};
-use na::{Translation3, UnitQuaternion, Vector3, Point3, Point2};
+use kiss3d::camera::{FixedView};
+use na::{Translation3, UnitQuaternion, Vector3, Point3};
 
 use std::f32::consts::PI;
 use std::path::Path;
+use std::ops::Add;
 
 use conrod::{Sizeable, Positionable, Labelable, Widget, widget_ids};
 
-const UI_WIDTH_PIXELS: f64 = 350.;
-const AXE_LENGTH_NORMALIZED: f32 = 0.24;
+const UI_WIDTH_P: f64 = 150.;
+
+const AXE_LENGTH_N: f32 = 0.24;
+
+const Y_INIT_POS_N: f32 = -0.06;
+const Z_INIT_POS_N: f32 = -0.4;
+
+const Y_INIT_ROT: f32 = -45.0 / 180.0 * PI;
 
 struct Rotation {
     x_angle: f32,
@@ -48,67 +54,90 @@ widget_ids! {
     }
 }
 
-// nomalized witdh
-fn w_n(target_w: f32, window_h: f32) -> f32 {
-    target_w / window_h
-}
-
 fn draw_ui(ui_cell: &mut UiCell, ids: &Ids, rot: &mut Rotation) {
+    let slider_w_p = 16.0;
+    let slider_h_p = 180.0;
+    let sliders_gap_p = 33.0;
+    let font_size = 11;
+
     widget::Canvas::new()
         .align_left()
-        .w(UI_WIDTH_PIXELS)
+        .w(UI_WIDTH_P)
         .rgb(1.0, 1.0, 1.0)
         .border_rgb(1.0, 1.0, 1.0)
         .set(ids.canvas, ui_cell);
 
     for v in widget::Slider::new(rot.x_angle, -180.0, 180.0)
         .label("X")
-        .w(200.)
-        .h(25.)
-        .up_from(ids.slider_y, 20.)
+        .w(slider_w_p)
+        .h(slider_h_p)
+        .left_from(ids.slider_y, sliders_gap_p)
         .set(ids.slider_x, ui_cell) 
     {
         rot.x_angle = v;
     }
 
-    widget::Text::new(&format!("{:.2}", rot.x_angle))
-        .right_from(ids.slider_x, 10.)
+    widget::Text::new(&format!("{:.1}°", rot.x_angle))
+        .font_size(font_size)
+        .down_from(ids.slider_x, 10.)
+        .align_middle_x_of(ids.slider_x)
         .set(ids.angle_x, ui_cell);
 
     for v in widget::Slider::new(rot.y_angle, -180.0, 180.0)
         .label("Y")
-        .w(200.)
-        .h(25.)
+        .w(slider_w_p)
+        .h(slider_h_p)
         .middle_of(ids.canvas)
         .set(ids.slider_y, ui_cell) 
     {
         rot.y_angle = v;
     }
 
-    widget::Text::new(&format!("{:.2}", rot.y_angle))
-        .right_from(ids.slider_y, 10.)
+    widget::Text::new(&format!("{:.1}°", rot.y_angle))
+        .font_size(font_size)
+        .down_from(ids.slider_y, 10.)
+        .align_middle_x_of(ids.slider_y)
         .set(ids.angle_y, ui_cell);
 
     for v in widget::Slider::new(rot.z_angle, -180.0, 180.0)
         .label("Z")
-        .w(200.)
-        .h(25.)
-        .down_from(ids.slider_y, 20.)
+        .w(slider_w_p)
+        .h(slider_h_p)
+        .right_from(ids.slider_y, sliders_gap_p)
         .set(ids.slider_z, ui_cell) 
     {
         rot.z_angle = v;
     }
 
-    widget::Text::new(&format!("{:.2}", rot.z_angle))
-        .right_from(ids.slider_z, 10.)
+    widget::Text::new(&format!("{:.1}°", rot.z_angle))
+        .font_size(font_size)
+        .down_from(ids.slider_z, 10.)
+        .align_middle_x_of(ids.slider_z)
         .set(ids.angle_z, ui_cell);
 }
 
-fn draw_axes(window: &mut Window, x_shift: f32) {
-    let half_axe = AXE_LENGTH_NORMALIZED / 2.0;
-    window.draw_line(&Point3::new(x_shift + half_axe, 0.0, 0.0), &Point3::new(x_shift - half_axe, 0.0, 0.0), &Point3::new(0.0, 0.0, 0.0));
-    window.draw_line(&Point3::new(x_shift, half_axe, 0.0), &Point3::new(x_shift, -half_axe, 0.0), &Point3::new(0.0, 0.0, 0.0));
-    window.draw_line(&Point3::new(x_shift, 0.0, half_axe), &Point3::new(x_shift, 0.0, -half_axe), &Point3::new(0.0, 0.0, 0.0));
+fn draw_axes(window: &mut Window) {
+    let half_axe = AXE_LENGTH_N / 2.0;
+
+    let init_shift = Vector3::new(0.0, Y_INIT_POS_N, Z_INIT_POS_N);
+
+    let init_rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), Y_INIT_ROT);
+
+    window.draw_line(
+        &init_rot.transform_point(&Point3::new(half_axe, 0.0, 0.0)).add(init_shift),
+        &init_rot.transform_point(&Point3::new(-half_axe, 0.0, 0.0)).add(init_shift), 
+        &Point3::new(0.0, 0.0, 0.0)
+    );
+    window.draw_line(
+        &Point3::new(0.0, half_axe, 0.0).add(init_shift), 
+        &Point3::new(0.0, -half_axe, 0.0).add(init_shift), 
+        &Point3::new(0.0, 0.0, 0.0)
+    );
+    window.draw_line(
+        &init_rot.transform_point(&Point3::new(0.0, 0.0, half_axe)).add(init_shift), 
+        &init_rot.transform_point(&Point3::new(0.0, 0.0, -half_axe)).add(init_shift), 
+        &Point3::new(0.0, 0.0, 0.0)
+    );
 }
 
 fn main() {
@@ -117,7 +146,11 @@ fn main() {
     window.set_light(Light::StickToCamera);
     window.set_background_color(1.0, 1.0, 1.0);
 
+    // Camera
+    let mut camera = FixedView::new();
+
     // State
+    let init_rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), Y_INIT_ROT);
     let mut rotation = Rotation {x_angle: 0.0, y_angle: 0.0, z_angle: 0.0};
 
     // Teapot
@@ -129,15 +162,12 @@ fn main() {
     // UI
     let ids = Ids::new(window.conrod_ui_mut().widget_id_generator());
 
-    while window.render() {
-        let window_h = window.height() as f32;
+    while window.render_with_camera(&mut camera) {
 
-        let x_translation = -w_n(UI_WIDTH_PIXELS as f32, window_h) / 2.;
-        
-        teapot.set_local_translation(Translation3::new(x_translation, 0.0, 0.0));
-        teapot.set_local_rotation(rotation.x() * rotation.y() * rotation.z());
+        teapot.set_local_translation(Translation3::new(0.0, Y_INIT_POS_N, Z_INIT_POS_N));
+        teapot.set_local_rotation(init_rot * rotation.x() * rotation.y() * rotation.z());
 
-        draw_axes(&mut window, x_translation);
+        draw_axes(&mut window);
 
         let mut ui_cell = window.conrod_ui_mut().set_widgets();
         draw_ui(&mut ui_cell, &ids, &mut rotation);
